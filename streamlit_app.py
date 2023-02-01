0f30e02b2b69d3c25895be9fdb7d273642e08924
michaelakerr <michaela.kerrr@gmail.com>
2023-02-01 22:19:11 +1300

    Update data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,55 @@
-from collections import namedtuple
-import altair as alt
-import math
+import datetime
+
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def update_first_number():
+    day = st.session_state.date.day
+    month = st.session_state.date.month
+    if day < 10:
+        if month < 10:
+            st.session_state.date = '0{}0{}0000'.format(month, day)
+        else:
+            st.session_state.date = '{}0{}0000'.format(month, day)
+    else:
+        st.session_state.date = '{}0{}0000'.format(month, day)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def update_number():
+    day = st.session_state.select_input.day
+    month = st.session_state.select_input.month
+    if day < 10:
+        if month < 10:
+            st.session_state.date = '0{}0{}0000'.format(month, day)
+        else:
+            st.session_state.date = '{}0{}0000'.format(month, day)
+    else:
+        st.session_state.date = '0{}{}0000'.format(month, day)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# https://github.com/mkhorasani/Streamlit-Authenticator
+if "date" not in st.session_state:
+    st.session_state.date = datetime.datetime(2020, 7, 4).date()
+    update_first_number()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+df = pd.read_csv('data/{}.csv'.format(st.session_state.date), skipfooter=1, skiprows=16,
+                 parse_dates=True, usecols=['Date', 'P_Avg[W]'])
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+df['Date'] = pd.to_datetime(df['Date'])
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+selectedDate = df['Date'][1].date()
+
+
+# APP
+
+st.title('Chia Solar Monitoring App', anchor=None)
+st.subheader('Usage for {}'.format(selectedDate))
+
+st.session_state.select_input = st.date_input(
+    'Selected Date', selectedDate, on_change=update_number)
+# st.write('Your birthday is:', d)
+
+st.line_chart(df[['Date', 'P_Avg[W]']], x='Date')
+st.dataframe(df)
